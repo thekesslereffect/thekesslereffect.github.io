70741d1198125e607674dda8883c535bd99afa3c
thekesslereffect <eldron2323@gmail.com>
2022-06-07 17:29:18 -0700

    background parallax

--- Build/Builds.framework.js
+++ Build/Builds.framework.js
@@ -1292,7 +1292,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2321632;
+STATICTOP = STATIC_BASE + 2322288;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3362,7 +3362,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2321632;
+var STATIC_BUMP = 2322288;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
